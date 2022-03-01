675b36c6a83ac5261ae9e9bee83adaaf5ca0612b
Shreya Shankar <ss.shankar505@gmail.com>
2022-03-01 10:20:55 -0800

    Adding intel sensor data

--- example.py
+++ example.py
@@ -7,3 +7,11 @@ if __name__ == "__main__":
     # Query dataset
     df = dataset.loadRecent(timedelta(days=5))
     print(df.head())
+
+    intel_lab_data = Dataset(
+        "intel_lab_data", cutoff_date="2004-03-04", backend="pandas"
+    )
+
+    # Query dataset
+    df = intel_lab_data.loadRecent(timedelta(days=5))
+    print(df.head())
